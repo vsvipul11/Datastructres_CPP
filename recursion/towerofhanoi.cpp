3d4339f750f8b04e9139c8a2db9cbf74ed8b5e54
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-02-25 14:52:38 +0530

    Add files via upload

---
+++ recursion/towerofhanoi.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+void tower(int n, int a , int c , int b){
+    
+    if(n==1){
+        cout<<" move "<<n<<" form "<<a <<" to "<<c<<endl;
+        return;
+    }
+    
+    tower( n-1 ,  a, b, c);
+    
+    cout<<" move "<<n<<" from "<<a<<" to "<<c<<endl;
+    
+    tower( n-1 ,  b ,  c ,  a);
+        
+}
+
+int main(){
+    int n = 3;
+    int a =1;
+    int b = 2;
+    int c = 3;
+     
+     tower(n , a,c,b);
+    
+}

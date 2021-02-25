5e9083d43308bde2ce6b4823d30cdf3a4f6caa46
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-02-25 18:36:55 +0530

    Create factorials.cpp
    input - n =5
    output - factorial of 5 is 120

---
+++ recursion/factorials.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+int fact(int n){
+    if(n==1){
+        return 1;
+    }
+    
+    int fac = fact(n-1);
+        return n*fac;
+    
+}
+
+int main(){
+    int n;
+  cin>>n;
+    
+    cout<<"Factorial of "<<n<<" is " fact(n)<<endl;
+    return 0;
+}

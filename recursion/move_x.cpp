4ace68bda4325b3ee6cea747bc10a12e4706a118
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-03-01 10:31:08 +0530

    Create move_x.cpp
    Given a string you  have to move all the x to the end of the string.
    input - abccxfxdxxxse
    output - abfdsexxxxx

---
+++ recursion/move_x.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+string movex(string s){
+    if(s.length() == 0){
+        return "";
+    }
+    
+    
+    char ch = s[0];
+    string ans =  movex(s.substr(1));
+     
+     if(ch == 'x'){
+         return ans + ch;
+     }
+    return  ch + ans;
+    
+    
+}
+
+int main() {
+	  string s;
+	  
+	  cin>>s;
+	  cout<<movex(s);
+	return 0;
+}

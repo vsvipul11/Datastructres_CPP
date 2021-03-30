9f5635fd29ea3ecd567cfdb7e43207cd943012a6
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-03-30 12:57:11 +0530

    Create bfs.cpp
    BFS algorithm for level order print

---
+++ Trees/bfs.cpp
@@ -0,0 +1,65 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class node{
+  public:
+  int data;
+  node* left;
+  node* right;
+    
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+        
+    }
+};
+node* buildtree(){
+    int d;
+    cin>>d;
+    
+    if(d==-1){
+        return NULL;
+    }
+    node* root = new node(d);
+   root->left = buildtree();
+    root->right = buildtree();
+    
+    return root;
+    
+    
+}
+
+
+void bfs(node* root){
+     
+    queue<node*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        node* f = q.front();
+        cout<<f->data<<" ";
+        q.pop();
+        
+    if(f->left){
+        q.push(f->left);
+    }    
+      if(f->right){
+          q.push(f->right);
+      }  
+    }
+    
+ return ;   
+    
+    
+    
+}
+
+
+
+int main() {
+	node* root = buildtree();
+	bfs(root);
+	return 0;
+}

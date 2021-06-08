ae6eab0a0dc28958e6dc6d407e09bf32a6fc4b48
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-06-08 15:36:38 +0530

    Update and rename create.cpp to create_linkedlist.cpp

---
+++ Linked List/create_linkedlist.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class node{
+  public:
+      int data;
+      node* next;
+  
+    node(int d){
+        data = d;
+        next = NULL;
+    }
+
+};
+
+void insert(node* &head , int d){
+    
+
+node* n = new node(d);    
+   
+   if(head == NULL){
+       head = n;
+       return;
+   } 
+    
+    node* temp = head;
+    
+    while(temp->next){
+        temp = temp->next;
+        
+    }
+    temp->next = n;
+    
+}
+
+void print(node* head){
+    
+    node* temp = head;
+    
+    while(temp){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+}
+
+
+int main() {
+node* head = NULL;
+
+int n,d;
+cin>>n;
+
+for(int  i = 0 ; i< n ; i++){
+    cin>>d;
+    insert(head  , d);
+}
+print(head);
+	return 0;
+}
+

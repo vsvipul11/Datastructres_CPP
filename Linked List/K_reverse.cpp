bc8d8849408e64d3adf9bc2b528d7cb7c83add2c
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-06-12 18:20:07 +0530

    Update K_reverse.cpp
    input = 1-2-3-4-5-6
    k = 2
    output = 2-1-4-3-6-5

--- Linked List/K_reverse.cpp
+++ Linked List/K_reverse.cpp
@@ -1,1 +1,26 @@
-
+node* kreverse(node* head, int k){
+    
+    if(head == NULL){
+        return NULL;
+    }
+    
+    node* prev = NULL;
+    node* curr = head;
+    node* temp;
+    int count = 1;
+    
+    while(curr!=NULL and count <=k ){
+        temp = curr->next;
+        
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+        count++;
+    }
+    if(temp !=NULL){
+        head->next = kreverse(temp , k);
+    }
+    
+    return prev;
+    
+}

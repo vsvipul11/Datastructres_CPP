8de106124cae04254969e992168399f97fc3153f
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-06-19 23:43:58 +0530

    Update merge_two_sorted_array.cpp

--- ARRAYS/merge_two_sorted_array.cpp
+++ ARRAYS/merge_two_sorted_array.cpp
@@ -1,1 +1,34 @@
-
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>num(m);
+        for(int i=0;i<m;i++){
+            num[i]=nums1[i];
+        }
+        
+        int i=0,j=0,z=0;
+        while(i<m && j<n){
+            if(num[i]<nums2[j]){
+                nums1[z]=num[i];
+                i++;
+                z++;
+            }else{
+                nums1[z]=nums2[j];
+                z++;
+                j++;
+            }
+        }
+        
+        while(i<m){
+            nums1[z]=num[i];
+            z++;
+            i++;
+        }
+        
+        while(j<n){
+            nums1[z]=nums2[j];
+            z++;
+            j++;
+        }                 
+    }
+};

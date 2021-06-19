686a920edc91f19f822a838b40e0683d84a1f68d
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2021-06-19 23:43:16 +0530

    Update Kadanesalgo.cpp

--- Dynamic Programming/Kadanesalgo.cpp
+++ Dynamic Programming/Kadanesalgo.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size(); // size array
+        int sum = 0; // sum curent of array
+        int max_sum = nums[0]; // MAX subarray in array
+        for(int i =0;i<n;i++)
+        {
+            if(sum < 0)
+            {
+                sum = 0;
+            }
+            sum +=nums[i];
+            //sum = max(sum,0);
+            max_sum = max(sum,max_sum);            
+        }
+        return max_sum;
+    }
+};
